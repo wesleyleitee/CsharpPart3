b6080e5346b2045bd6b96dc5b8d9e93338a7290e
Wesley Leite <leitewesleyleite@gmail.com>
2024-08-27 19:32:22 -0300

    Conditional - Tipo de fornecedor

--- CsharpPart3/src/ByteBank/Program.cs
+++ CsharpPart3/src/ByteBank/Program.cs
@@ -1,10 +1,10 @@
 ﻿using ByteBank.Funcionarios;
 
-Funcionario carlos = new Funcionario();
+Funcionario carlos = new Funcionario(1);
 
 carlos.Nome = "Carlos";
 carlos.CPF = "546.456.456-20";
-carlos.Salario = 8000;
+carlos.Salario = 2000;
 
 Console.WriteLine(carlos.Nome);
 Console.WriteLine(carlos.GetBonificacao());
