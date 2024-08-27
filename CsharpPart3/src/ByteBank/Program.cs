a8edaa990348419d3ecfca3f5712b9af24e3732d
Wesley Leite <leitewesleyleite@gmail.com>
2024-08-27 19:49:04 -0300

    Functions simple

--- CsharpPart3/src/ByteBank/Program.cs
+++ CsharpPart3/src/ByteBank/Program.cs
@@ -1,11 +1,21 @@
 ﻿using ByteBank.Funcionarios;
 
 Funcionario carlos = new Funcionario();
-
 carlos.Nome = "Carlos";
 carlos.CPF = "546.456.456-20";
 carlos.Salario = 2000;
 
-Console.WriteLine(carlos.Nome);
-Console.WriteLine(carlos.GetBonificacao());
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta";
+roberta.CPF = "789.789.798-30";
+roberta.Salario = 5000;
+
+
+
+Console.WriteLine($"Nome: {carlos.Nome}");
+Console.WriteLine($"Bonificação {carlos.GetBonificacao()}");
+
+Console.WriteLine($"Nome: {roberta.Nome}");
+Console.WriteLine($"Bonificação {roberta.GetBonificacao()}");
+
 Console.ReadLine();
