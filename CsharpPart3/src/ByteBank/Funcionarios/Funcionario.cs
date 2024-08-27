fd4a4eca75df913f0bd8ce1a7a74ea189af152c2
Wesley Leite <leitewesleyleite@gmail.com>
2024-08-27 19:32:22 -0300

    Conditional - Supplier type

--- CsharpPart3/src/ByteBank/Funcionarios/Funcionario.cs
+++ CsharpPart3/src/ByteBank/Funcionarios/Funcionario.cs
@@ -8,12 +8,30 @@ namespace ByteBank.Funcionarios
 {
     public class Funcionario
     {
+        // 0 - Funcionario
+        // 1 - Diretor
+        // 2 - Designer
+        // 3 - Gerente de Conta Corrente
+        // 4 - Coordenador
+
+        private int _tipo;
+
         public string Nome { get; set; }
         public string CPF { get; set; }
         public double Salario{ get; set; }
-        
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
         public double GetBonificacao()
         {
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+
             return Salario * 0.1;
         }
     }
